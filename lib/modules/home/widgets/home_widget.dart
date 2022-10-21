21e4bcd1a8648d057dac44d3a6eff0adb9039595
Besan <besan.basheer3@gmail.com>
2022-10-21 22:36:22 +0300

    intial commit

---
+++ lib/modules/home/widgets/home_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:loan_app/modules/home/models/home_model.dart';
+
+import '../views/pay_loan_screen.dart';
+
+class HomeCard extends StatelessWidget {
+  HomeModel homeModel;
+  HomeCard({
+    required this.homeModel
+  });
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (() => Get.to(LoanScreen())),
+      child: Container(
+        width: 335.w,
+        height: 97.5.h,
+        child: Card(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.r),
+              side: BorderSide(color: Color(0xff244AD3).withOpacity(0.5))),
+          child: ListTile(
+            leading: Image.asset(
+              homeModel.image!,
+              width: 65.w,
+              height: 65.h,
+            ),
+            title: Text(homeModel.name!),
+            subtitle: Text(homeModel.number!),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/home/widgets/notification_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:loan_app/modules/home/models/notification_model.dart';
+import 'package:loan_app/shared/components/components.dart';
+
+class NotificationsItem extends StatelessWidget {
+  NotificationModel notificationModel;
+  NotificationsItem(this.notificationModel);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 337.w,
+      child: Column(
+        children: [
+          ListTile(
+            leading: Image.asset(notificationModel.image!),
+            title: Text(notificationModel.name!),
+            subtitle: Text(
+              notificationModel.description!,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: Text(notificationModel.time!),
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(width: 150.w, child: DefaultCard(text:'Reject', width: 150.w,)),
+              Container(width: 150.w, child: DefaultCard(text:'Reject', width: 150.w)),
+            ],
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+        ],
+      ),
+    );
+  }
+}
